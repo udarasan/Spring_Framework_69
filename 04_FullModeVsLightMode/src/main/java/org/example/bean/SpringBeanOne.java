ed00d748143b81259746f1beb64b5815da1158fe
udarasan <udarassanjeewa@gmail.com>
2025-01-30 09:39:04 +0530

    spring full mode vs light mode

--- 04_FullModeVsLightMode/src/main/java/org/example/bean/SpringBeanOne.java
+++ 04_FullModeVsLightMode/src/main/java/org/example/bean/SpringBeanOne.java
@@ -1,10 +1,20 @@
 package org.example.bean;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SpringBeanOne {
-    public SpringBeanOne() {
-        System.out.println("SpringBeanOne Constructor");
+    @Bean
+    public SpringBeanTwo getSpringBeanTwo() {
+        SpringBeanThree springBeanThree1 = getSpringBeanThree();
+        SpringBeanThree springBeanThree2 = getSpringBeanThree();
+        System.out.println(springBeanThree1);
+        System.out.println(springBeanThree2);
+        return new SpringBeanTwo();
+    }
+    @Bean
+    public SpringBeanThree getSpringBeanThree() {
+        return new SpringBeanThree();
     }
 }
--- 04_FullModeVsLightMode/src/main/java/org/example/config/AppConfig.java
+++ 04_FullModeVsLightMode/src/main/java/org/example/config/AppConfig.java
@@ -1,9 +1,24 @@
 package org.example.config;
 
+import org.example.bean.SpringBeanThree;
+import org.example.bean.SpringBeanTwo;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "org.example.bean")
 public class AppConfig {
+/*    @Bean
+    public SpringBeanTwo getSpringBeanTwo() {
+        SpringBeanThree springBeanThree1 = getSpringBeanThree();
+        SpringBeanThree springBeanThree2 = getSpringBeanThree();
+        System.out.println(springBeanThree1);
+        System.out.println(springBeanThree2);
+        return new SpringBeanTwo();
+    }
+    @Bean
+    public SpringBeanThree getSpringBeanThree() {
+        return new SpringBeanThree();
+    }*/
 }
