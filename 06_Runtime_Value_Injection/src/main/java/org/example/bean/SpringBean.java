3ae7d90257ac254fe0df452036d00b25b68981ad
udarasan <udarassanjeewa@gmail.com>
2025-01-30 11:07:23 +0530

    runtime value injection

--- 06_Runtime_Value_Injection/src/main/java/org/example/bean/SpringBean.java
+++ 06_Runtime_Value_Injection/src/main/java/org/example/bean/SpringBean.java
@@ -1,10 +1,43 @@
 package org.example.bean;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SpringBean {
+public class SpringBean implements InitializingBean {
+    @Value("udara-san")
+    private String name;
+
     public SpringBean() {
         System.out.println("Spring Bean Constructor");
+        System.out.println(name);
     }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println(name);
+    }
+
+
+
+
+
+
+    /*@Autowired(required = false)
+    public SpringBean(@Value("udara-san") String name, @Value("1") int id) {
+        System.out.println("Spring Bean Constructor");
+        System.out.println(name);
+        System.out.println(id);
+    }
+    @Autowired(required = false)
+    public SpringBean(@Value("udara-san") String name,
+                      @Value("1") int id,@Value("true")boolean b) {
+        System.out.println("Spring Bean Constructor");
+        System.out.println(name);
+        System.out.println(id);
+        System.out.println(b);
+    }*/
+
 }
