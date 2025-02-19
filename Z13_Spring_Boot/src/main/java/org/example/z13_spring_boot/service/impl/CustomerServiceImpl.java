0f82cb420accb5169b61ebb1a43437e04fca9f96
udarasan <udarassanjeewa@gmail.com>
2025-02-19 11:41:11 +0530

    model mapper

--- Z13_Spring_Boot/src/main/java/org/example/z13_spring_boot/service/impl/CustomerServiceImpl.java
+++ Z13_Spring_Boot/src/main/java/org/example/z13_spring_boot/service/impl/CustomerServiceImpl.java
@@ -4,6 +4,8 @@ import org.example.z13_spring_boot.dto.CustomerDTO;
 import org.example.z13_spring_boot.entity.Customer;
 import org.example.z13_spring_boot.repo.CustomerRepo;
 import org.example.z13_spring_boot.service.CustomerService;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,24 +17,26 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Autowired
     private CustomerRepo customerRepo;
+    @Autowired
+    private ModelMapper modelMapper;
 
     @Override
     public boolean addCustomer(CustomerDTO customerDTO) {
         if (customerRepo.existsById(customerDTO.getId())) {
             return false;
         }
-        Customer customer=new Customer(
+        /*Customer customer=new Customer(
                 customerDTO.getId(),
                 customerDTO.getName(),
-                customerDTO.getAddress());
-        customerRepo.save(customer);
+                customerDTO.getAddress());*/
+        customerRepo.save(modelMapper.map(customerDTO, Customer.class));
         return true;
     }
 
     @Override
     public boolean updateCustomer(CustomerDTO customerDTO) {
         if (customerRepo.existsById(customerDTO.getId())) {
-            customerRepo.save(new Customer(customerDTO.getId(), customerDTO.getName(), customerDTO.getAddress()));
+            customerRepo.save(modelMapper.map(customerDTO, Customer.class));
             return true;
         }
         return false;
@@ -45,13 +49,14 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public List<CustomerDTO> getAllCustomers() {
-        List<Customer>customerList=customerRepo.findAll();
+        /*List<Customer>customerList=customerRepo.findAll();
         List<CustomerDTO> customerDTOList=new ArrayList<>();
         for (Customer customer:customerList) {
             customerDTOList.add(
                     new CustomerDTO(customer.getId(),customer.getName(),customer.getAddress()));
-        }
-        return customerDTOList;
+        }*/
+        return modelMapper.map(customerRepo.findAll(),
+                new TypeToken<List<CustomerDTO>>() {}.getType());
     }
 
 
