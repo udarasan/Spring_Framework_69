ad895e91a0d5131af296bb640a51d67e5ba32f61
udarasan <udarassanjeewa@gmail.com>
2025-02-19 16:33:25 +0530

    transaction part

--- Z13_Spring_Boot/src/main/java/org/example/z13_spring_boot/service/impl/PlaceOrderImpl.java
+++ Z13_Spring_Boot/src/main/java/org/example/z13_spring_boot/service/impl/PlaceOrderImpl.java
@@ -1,7 +1,31 @@
 package org.example.z13_spring_boot.service.impl;
 
+import jakarta.transaction.Transactional;
+import org.example.z13_spring_boot.repo.ItemRepo;
+import org.example.z13_spring_boot.repo.OrderDetailRepo;
+import org.example.z13_spring_boot.repo.OrderRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Transactional
 public class PlaceOrderImpl {
+    @Autowired
+    private ItemRepo itemRepo;
+
+    @Autowired
+    private OrderRepo orderRepo;
+
+    @Autowired
+    private OrderDetailRepo orderDetailRepo;
+
+
+    void placeOrder() {
+        //qty management
+        itemRepo.updateQTY();
+        //persist - order
+        orderRepo.save();
+        //persist - order-detail
+        orderDetailRepo.save();
+    }
 }
